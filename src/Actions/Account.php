95a741b4547d82fff0efc3706300da6c7eb679a9
Levi <levi.durfee@gmail.com>
2016-12-03 15:53:54 -0500

    Adding Retrieve method to Account action

--- src/Actions/Account.php
+++ src/Actions/Account.php
@@ -2,6 +2,22 @@
 
 namespace wappr\DigitalOcean\Actions;
 
-class Account
+use Psr\Http\Message\ResponseInterface;
+use wappr\DigitalOcean\Contracts\Actions\RetrieveInterface;
+use wappr\DigitalOcean\Contracts\ClientInterface;
+
+/**
+ * Class Account.
+ */
+class Account implements RetrieveInterface
 {
+    /**
+     * @param ClientInterface $client
+     *
+     * @return ResponseInterface
+     */
+    public function retrieve(ClientInterface $client): ResponseInterface
+    {
+        return $client->get('account');
+    }
 }
