03efb906af63df7504856a2c64587980aa7869b7
Levi Durfee <levi.durfee@gmail.com>
2016-12-28 19:49:19 -0500

    [BlockStorage] Created another trait to reduce duplicate code

---
+++ src/BlockStorage/ByName.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace wappr\DigitalOcean\BlockStorage;
+
+trait ByName
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $region;
+
+    /**
+     * RetrieveBlockStorageByNameInterface constructor.
+     *
+     * @param string $name
+     * @param string $region
+     */
+    public function __construct(string $name, string $region)
+    {
+        $this->name = $name;
+        $this->region = $region;
+    }
+
+    /**
+     * @return array
+     */
+    public function getQuery(): array
+    {
+        return ['name' => $this->name, 'region' => $this->region];
+    }
+}
--- src/BlockStorage/Requests/RetrieveBlockStorageByNameRequest.php
+++ src/BlockStorage/Requests/RetrieveBlockStorageByNameRequest.php
@@ -2,6 +2,7 @@
 
 namespace wappr\DigitalOcean\BlockStorage\Requests;
 
+use wappr\DigitalOcean\BlockStorage\ByName;
 use wappr\DigitalOcean\Contracts\ModelInterface;
 use wappr\DigitalOcean\Contracts\BlockStorage\RetrieveBlockStorageByNameInterface;
 use wappr\DigitalOcean\Contracts\Requests\RequestModel;
@@ -11,33 +12,5 @@ use wappr\DigitalOcean\Contracts\Requests\RequestModel;
  */
 class RetrieveBlockStorageByNameRequest extends RequestModel implements ModelInterface, RetrieveBlockStorageByNameInterface
 {
-    /**
-     * @var string
-     */
-    protected $name;
-
-    /**
-     * @var string
-     */
-    protected $region;
-
-    /**
-     * RetrieveBlockStorageByNameInterface constructor.
-     *
-     * @param string $name
-     * @param string $region
-     */
-    public function __construct(string $name, string $region)
-    {
-        $this->name = $name;
-        $this->region = $region;
-    }
-
-    /**
-     * @return array
-     */
-    public function getQuery(): array
-    {
-        return ['name' => $this->name, 'region' => $this->region];
-    }
+    use ByName;
 }
