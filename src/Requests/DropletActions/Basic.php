a20b7913c7ebd988233e7bc76c30b774f438f8c0
Levi Durfee <levi.durfee@gmail.com>
2017-01-23 21:25:26 -0500

    [DropletActions] try and simplify

--- src/Requests/DropletActions/Basic.php
+++ src/Requests/DropletActions/Basic.php
@@ -2,7 +2,15 @@
 
 namespace wappr\digitalocean\Requests\DropletActions;
 
-class Basic
-{
+use wappr\digitalocean\Contracts\DropletActions\BasicContract;
+use wappr\digitalocean\Contracts\RequestContract;
 
+class Basic extends RequestContract implements BasicContract
+{
+    public $type;
+
+    public function __construct($type)
+    {
+        $this->type = $type;
+    }
 }
