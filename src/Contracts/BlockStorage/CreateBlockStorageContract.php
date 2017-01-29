d7d3fec9f77b1ef2a18726301082180f4d5f3e5a
Levi Durfee <levi.durfee@gmail.com>
2017-01-29 18:13:54 -0500

    [BlockStorage] Add Create Block Storage request

--- src/Contracts/BlockStorage/CreateBlockStorageContract.php
+++ src/Contracts/BlockStorage/CreateBlockStorageContract.php
@@ -2,6 +2,37 @@
 
 namespace wappr\digitalocean\Contracts\BlockStorage;
 
+/**
+ * Class CreateBlockStorage.
+ */
 interface CreateBlockStorageContract
 {
+    /**
+     * CreateBlockStorage constructor.
+     *
+     * @param $size_gigabytes
+     * @param $name
+     */
+    public function __construct($size_gigabytes, $name);
+
+    /**
+     * @param mixed $description
+     *
+     * @return CreateBlockStorage
+     */
+    public function setDescription($description);
+
+    /**
+     * @param mixed $region
+     *
+     * @return CreateBlockStorage
+     */
+    public function setRegion($region);
+
+    /**
+     * @param mixed $snapshot_id
+     *
+     * @return CreateBlockStorage
+     */
+    public function setSnapshotId($snapshot_id);
 }
--- src/Requests/BlockStorage/CreateBlockStorage.php
+++ src/Requests/BlockStorage/CreateBlockStorage.php
@@ -5,6 +5,67 @@ namespace wappr\digitalocean\Requests\BlockStorage;
 use wappr\digitalocean\Contracts\RequestContract;
 use wappr\digitalocean\Contracts\BlockStorage\CreateBlockStorageContract;
 
+/**
+ * Class CreateBlockStorage.
+ */
 class CreateBlockStorage extends RequestContract implements CreateBlockStorageContract
 {
+    /* Required */
+    public $size_gigabytes;
+    public $name;
+
+    /* Optional */
+    public $description;
+    public $region;
+    public $snapshot_id;
+
+    /**
+     * CreateBlockStorage constructor.
+     *
+     * @param $size_gigabytes
+     * @param $name
+     */
+    public function __construct($size_gigabytes, $name)
+    {
+        $this->size_gigabytes = $size_gigabytes;
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $description
+     *
+     * @return CreateBlockStorage
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $region
+     *
+     * @return CreateBlockStorage
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $snapshot_id
+     *
+     * @return CreateBlockStorage
+     */
+    public function setSnapshotId($snapshot_id)
+    {
+        $this->snapshot_id = $snapshot_id;
+
+        return $this;
+    }
 }
