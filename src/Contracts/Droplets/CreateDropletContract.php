a2404d6a2e5a39254a3ed0bbe5670cbdbb891aca
Levi Durfee <levi.durfee@gmail.com>
2017-02-06 19:36:43 -0500

    [Droplets] add docblocks for properties for create droplet

--- src/Contracts/Droplets/CreateDropletContract.php
+++ src/Contracts/Droplets/CreateDropletContract.php
@@ -4,6 +4,22 @@ namespace wappr\digitalocean\Contracts\Droplets;
 
 use wappr\digitalocean\Contracts\Requests;
 
+/**
+ * Interface CreateDropletContract.
+ *
+ * @property $name
+ * @property $region
+ * @property $size
+ * @property $image
+ * @property $ssh_keys
+ * @property $backups
+ * @property $ipv6
+ * @property $private_networking
+ * @property $user_data
+ * @property $monitoring
+ * @property $volume
+ * @property $tags
+ */
 interface CreateDropletContract extends Requests
 {
     public function __construct($name, $region, $size, $image);
