3519a0a42544cf6a3b6300ac7393ab0c1adf8660
Levi Durfee <levi.durfee@gmail.com>
2017-02-06 19:37:50 -0500

    [Droplets] add docblock properties to create multiple droplets

--- src/Contracts/Droplets/CreateMultipleContract.php
+++ src/Contracts/Droplets/CreateMultipleContract.php
@@ -4,6 +4,22 @@ namespace wappr\digitalocean\Contracts\Droplets;
 
 use wappr\digitalocean\Contracts\Requests;
 
+/**
+ * Interface CreateMultipleContract.
+ *
+ * @property $name
+ * @property $region
+ * @property $size
+ * @property $image
+ * @property $ssh_keys
+ * @property $backups
+ * @property $ipv6
+ * @property $private_networking
+ * @property $user_data
+ * @property $monitoring
+ * @property $volume
+ * @property $tags
+ */
 interface CreateMultipleContract extends Requests
 {
     public function __construct($names, $region, $size, $image);
